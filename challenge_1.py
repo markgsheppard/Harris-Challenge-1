e6b78820b50b416bcb0bf6b61a4ce3133cc225f3
Mark Sheppard <marksheppard@uchicago.edu>
2020-04-25 15:28:21 -0500

    Completed
    I'm unclear about Q3,, but the rest is meh.
    Thanks!

--- challenge_1.py
+++ challenge_1.py
@@ -8,18 +8,51 @@
 # to analyze supply and demand for bikes in the system. 
 
 # NOTE ** if you aren't able to run this, type "pip install json" into your command line
+import os
+os.system('cls' if os.name=='nt' else 'clear')
+"""clear console"""
+
 import json
+import requests
 
 # do not delete; this is the data you'll be working with
-divvy_stations = json.loads(open('divvy_stations.txt').read())
+#divvy_stations = json.loads(open('/Users/marksheppard/Documents/GitHub/PythonCourse/Harris-Challenge-1/divvy_stations.txt').read())
+
+gbfs_resp = requests.get('https://gbfs.divvybikes.com/gbfs/fr/station_status.json')
+gbfs_json = json.loads(gbfs_resp.text)
+
+divvy_stations_ = gbfs_json['data']['stations']
+
 
 # PROBLEM 1
 # find average number of empty docks (num_docks_available) and 
 # available bikes (num_bikes_available) at all stations in the system
 
+result_docks = []
+result_bikes = []
+result_rented = []
+result_e_bikes = []
+
+for data in divvy_stations_:
+    result_docks.append(data['num_docks_available'])
+    result_bikes.append(data['num_bikes_available'])
+    result_rented.append(data['is_renting'])
+    result_e_bikes.append(data['num_ebikes_available'])
+
+def average_():
+    av_dock = sum(result_docks)/len(result_docks)
+    av_bike = sum(result_bikes)/len(result_bikes)
+    print("The average number of docks available is: ~%.2f" % av_dock)
+    print("The average number of bikes available is: ~%.2f" % av_bike)
+average_()
 
 # PROBLEM 2
 # find ratio of bikes that are currently rented to total bikes in the system (ignore ebikes)
+
+def ratio_():
+    ratio_rented = sum(result_rented)*100/sum(result_bikes)-sum(result_e_bikes)
+    print("The ratio of bikes currently being rented is: ~%.2f" % ratio_rented + "%")
+ratio_()
 
 
 # PROBLEM 3 
@@ -27,3 +60,7 @@ divvy_stations = json.loads(open('divvy_stations.txt').read())
 # the percentage of bikes available at each individual station (again ignore ebikes). 
 # This variable should be formatted as a percentage rounded to 2 decimal places, e.g. 66.67%
 
+def available():
+    ratio_rented = 100 - (sum(result_rented)*100/sum(result_bikes)-sum(result_e_bikes))
+    print("The ratio of bikes currently available is: ~%.2f" % ratio_rented + "%")
+available()
